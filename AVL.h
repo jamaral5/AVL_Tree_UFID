417368584db82b769d5ef72ba24ab3a40d3d5503
jamaral5 <jakeamaral06@gmail.com>
2026-09-22 17:11:43 -0400

    overwrite previous header file with updated code
    added alll necesary helper functions.

--- AVL.h
+++ AVL.h
@@ -6,14 +6,15 @@
 #define UNTITLED1_AVL_H
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
 
 
 class AVLTree {
 public:
-    string insert(const string& name, const string& id);
-    string removeId(const string& id);
-    string searchId(const string& id);
+    string insert(const string& name, int id);
+    string removeId(int id);
+    string searchId(int id);
     string searchName(const string& name);
     vector<string> printInorder();
     vector<string> printPreorder();
@@ -29,8 +30,10 @@ private:
         int height;
     };
     Node* root = nullptr;
-    AVLTree::Node* helperInsert(Node* helpRoot, int key);
+    AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, const string& name, int id);
+    AVLTree::Node* AVLTree::helperRemove(Node* helpRoot, int id);
     void helperInorder(Node* helpRoot);
+    AVLTree::Node* helperSearch(Node* helpRoot, int id);
 };
 
 void AVLTree::helperInorder(Node* helpRoot) {
@@ -43,18 +46,63 @@ void AVLTree::helperInorder(Node* helpRoot) {
     }
 };
 
-AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, int key) {
+AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, const string& name, int id) {
     if (helpRoot == nullptr) {
-        return new Node(key);
+        return new Node{name, id, nullptr, nullptr, 0};
     }
-    if (key < helpRoot->val) {
-        helpRoot->left = helperInsert(helpRoot->left, key);
+    if (id < helpRoot->val) {
+        helpRoot->left = helperInsert(helpRoot->left, name, id);
     }else {
-        helpRoot->right = helperInsert(helpRoot->right, key);
+        helpRoot->right = helperInsert(helpRoot->right, name, id);
     }
     return helpRoot;
 };
 
+AVLTree::Node* AVLTree::helperSearch(Node* helpRoot, int id) {
+    if (helpRoot == nullptr) {
+        return nullptr;
+    }
+    if (id < helpRoot->val) {
+        return helperSearch(helpRoot->left, id);
+    }
+    if (id > helpRoot->val) {
+        return helperSearch(helpRoot->right, id);
+    }
+    return helpRoot;
+};
 
-
+AVLTree::Node* AVLTree::helperRemove(Node* helpRoot, int id) {
+    if (helpRoot == nullptr) {
+        return nullptr;
+    }
+    if (id < helpRoot->val) {
+        helpRoot->left = helperRemove(helpRoot->left, id);
+        return helpRoot;
+    }
+    if (id > helpRoot->val) {
+        helpRoot->right = helperRemove(helpRoot->right, id);
+        return helpRoot;
+    }
+    if (id == helpRoot->val) {
+        if (helpRoot->left == nullptr && helpRoot->right == nullptr) {
+            delete helpRoot;
+            return nullptr;
+        }else if(helpRoot->left == nullptr) {
+            Node* temp = helpRoot->right;
+            delete helpRoot->right;
+            return temp;
+        }else if(helpRoot->right == nullptr) {
+           Node* temp = helpRoot->left;
+            delete helpRoot;
+            return temp;
+        }
+    }
+    while (helpRoot->right->left != nullptr) {
+        helpRoot->right = helpRoot->right->left;
+    }
+    helpRoot->val = helpRoot->right->val;
+    helpRoot->name = helpRoot->right->name;
+    helpRoot->right = helperRemove(helpRoot->right, helpRoot->right->val);
+    return helpRoot;
+};
 #endif //UNTITLED1_AVL_H
