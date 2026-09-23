ff4be6bca1577bd8e0930d8f37666c672f83dc45
jamaral5 <jakeamaral06@gmail.com>
2026-09-22 20:55:33 -0400

    just leaving the declarations.
    it wasnt working before, and clion warned of this

--- AVL.h
+++ AVL.h
@@ -13,14 +13,14 @@ using namespace std;
 class AVLTree {
 public:
     string insert(const string& name, int id);
-    string removeId(int id);
+    string remove(int id);
     string searchId(int id);
     string searchName(const string& name);
-    vector<string> printInorder();
-    vector<string> printPreorder();
-    vector<string> printPostorder();
+    string removeInorder(int n);
+    void printInorder();
+    void printPreorder();
+    void printPostorder();
     int printLevelCount();
-    int removeInorder(const string& level);
 
 private:
     struct Node{
@@ -28,81 +28,18 @@ private:
         Node* left;
         Node* right;
         int height;
+        string name;
     };
     Node* root = nullptr;
-    AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, const string& name, int id);
-    AVLTree::Node* AVLTree::helperRemove(Node* helpRoot, int id);
+    Node* helperInsert(Node* helpRoot, const string& name, int id);
+    Node* helperRemove(Node* helpRoot, int id);
+    Node* helperSearch(Node* helpRoot, int id);
     void helperInorder(Node* helpRoot);
-    AVLTree::Node* helperSearch(Node* helpRoot, int id);
+    void helperPreorder(Node* helpRoot);
+    void helperPostorder(Node* helpRoot);
+    void helperInordersort(Node* helpRoot, vector<int>& vals);
+    void helperSearchname(Node* helpRoot, string name, vector<string>& vals);
+    int helperMax(Node* helpRoot);
 };
 
-void AVLTree::helperInorder(Node* helpRoot) {
-    if (helpRoot == nullptr) {
-        cout << "";
-    }else {
-        helperInorder(helpRoot->left);
-        cout << helpRoot->val << " ";
-        helperInorder(helpRoot->right);
-    }
-};
-
-AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, const string& name, int id) {
-    if (helpRoot == nullptr) {
-        return new Node{name, id, nullptr, nullptr, 0};
-    }
-    if (id < helpRoot->val) {
-        helpRoot->left = helperInsert(helpRoot->left, name, id);
-    }else {
-        helpRoot->right = helperInsert(helpRoot->right, name, id);
-    }
-    return helpRoot;
-};
-
-AVLTree::Node* AVLTree::helperSearch(Node* helpRoot, int id) {
-    if (helpRoot == nullptr) {
-        return nullptr;
-    }
-    if (id < helpRoot->val) {
-        return helperSearch(helpRoot->left, id);
-    }
-    if (id > helpRoot->val) {
-        return helperSearch(helpRoot->right, id);
-    }
-    return helpRoot;
-};
-
-AVLTree::Node* AVLTree::helperRemove(Node* helpRoot, int id) {
-    if (helpRoot == nullptr) {
-        return nullptr;
-    }
-    if (id < helpRoot->val) {
-        helpRoot->left = helperRemove(helpRoot->left, id);
-        return helpRoot;
-    }
-    if (id > helpRoot->val) {
-        helpRoot->right = helperRemove(helpRoot->right, id);
-        return helpRoot;
-    }
-    if (id == helpRoot->val) {
-        if (helpRoot->left == nullptr && helpRoot->right == nullptr) {
-            delete helpRoot;
-            return nullptr;
-        }else if(helpRoot->left == nullptr) {
-            Node* temp = helpRoot->right;
-            delete helpRoot->right;
-            return temp;
-        }else if(helpRoot->right == nullptr) {
-           Node* temp = helpRoot->left;
-            delete helpRoot;
-            return temp;
-        }
-    }
-    while (helpRoot->right->left != nullptr) {
-        helpRoot->right = helpRoot->right->left;
-    }
-    helpRoot->val = helpRoot->right->val;
-    helpRoot->name = helpRoot->right->name;
-    helpRoot->right = helperRemove(helpRoot->right, helpRoot->right->val);
-    return helpRoot;
-};
 #endif //UNTITLED1_AVL_H
