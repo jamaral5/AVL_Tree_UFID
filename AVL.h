7e65c9b0a5ffcd0242de2040fb8d8baebef1e307
jamaral5 <jakeamaral06@gmail.com>
2026-09-23 15:12:41 -0400

    Add helper functions for AVL tree operations
    Implemented helper functions for AVL tree operations including insert, remove, search, and traversal methods.

--- AVL.h
+++ AVL.h
@@ -2,44 +2,192 @@
 // Created by jakea on 9/18/2026.
 //
 
-#ifndef UNTITLED1_AVL_H
-#define UNTITLED1_AVL_H
-#include <string>
+#include "AVL.h"
 #include <vector>
-#include <iostream>
-using namespace std;
 
+//HELP FUNCTIONS
 
-class AVLTree {
-public:
-    string insert(const string& name, int id);
-    string remove(int id);
-    string searchId(int id);
-    string searchName(const string& name);
-    string removeInorder(int n);
-    void printInorder();
-    void printPreorder();
-    void printPostorder();
-    int printLevelCount();
-
-private:
-    struct Node{
-        int val;
-        Node* left;
-        Node* right;
-        int height;
-        string name;
-    };
-    Node* root = nullptr;
-    Node* helperInsert(Node* helpRoot, const string& name, int id);
-    Node* helperRemove(Node* helpRoot, int id);
-    Node* helperSearch(Node* helpRoot, int id);
-    void helperInorder(Node* helpRoot);
-    void helperPreorder(Node* helpRoot);
-    void helperPostorder(Node* helpRoot);
-    void helperInordersort(Node* helpRoot, vector<int>& vals);
-    void helperSearchname(Node* helpRoot, string name, vector<string>& vals);
-    int helperMax(Node* helpRoot);
+void AVLTree::helperPreorder(Node* helpRoot) {
+    if (helpRoot == nullptr) {
+        return;
+    }
+    cout << helpRoot->val << " ";
+    helperPreorder(helpRoot->left);
+    helperPreorder(helpRoot->right);
 };
 
-#endif //UNTITLED1_AVL_H
+void AVLTree::helperPostorder(Node* helpRoot) {
+    if (helpRoot == nullptr) {
+        return;
+    }
+    helperPostorder(helpRoot->left);
+    helperPostorder(helpRoot->right);
+    cout << helpRoot->val << " ";
+};
+
+
+void AVLTree::helperInorder(Node* helpRoot) {
+    if (helpRoot == nullptr) {
+        return;
+    }
+    helperInorder(helpRoot->left);
+    cout << helpRoot->val << " ";
+    helperInorder(helpRoot->right);
+};
+
+void AVLTree::helperInordersort(Node* helpRoot, vector<int>& vals) {
+    if (helpRoot == nullptr) {
+        return;
+    }
+    helperInordersort(helpRoot->left, vals);
+    vals.push_back(helpRoot->val);
+    helperInordersort(helpRoot->right, vals);
+
+};
+
+AVLTree::Node* AVLTree::helperInsert(Node* helpRoot, const string& name, int id) {
+    if (helpRoot == nullptr) {
+        return new Node{id, nullptr, nullptr, 0, name};
+    }
+    if (id < helpRoot->val) {
+        helpRoot->left = helperInsert(helpRoot->left, name, id);
+    }else {
+        helpRoot->right = helperInsert(helpRoot->right, name, id);
+    }
+    return helpRoot;
+};
+
+AVLTree::Node* AVLTree::helperSearch(Node* helpRoot, int id) {
+    if (helpRoot == nullptr) {
+        return nullptr;
+    }
+    if (id < helpRoot->val) {
+        return helperSearch(helpRoot->left, id);
+    }
+    if (id > helpRoot->val) {
+        return helperSearch(helpRoot->right, id);
+    }
+    return helpRoot;
+};
+
+void AVLTree::helperSearchname(Node* helpRoot, string name, vector<string>& vals) {
+    if (helpRoot == nullptr) {
+        return;
+    }
+    helperSearchname(helpRoot->left, name, vals);
+    if (helpRoot->name == name) {
+        vals.push_back(to_string(helpRoot->val));
+    }
+    helperSearchname(helpRoot->right, name, vals);
+};
+
+int AVLTree::helperMax(Node* helpRoot) {
+    if (helpRoot == nullptr) {
+        return 0;
+    }
+    return 1 + max(helperMax(helpRoot->left), helperMax(helpRoot->right));
+};
+
+AVLTree::Node* AVLTree::helperRemove(Node* helpRoot, int id) {
+    if (helpRoot == nullptr) {
+        return nullptr;
+    }
+    if (id < helpRoot->val) {
+        helpRoot->left = helperRemove(helpRoot->left, id);
+        return helpRoot;
+    }
+    if (id > helpRoot->val) {
+        helpRoot->right = helperRemove(helpRoot->right, id);
+        return helpRoot;
+    }
+    if (id == helpRoot->val) {
+        if (helpRoot->left == nullptr && helpRoot->right == nullptr) {
+            delete helpRoot;
+            return nullptr;
+        }else if(helpRoot->left == nullptr) {
+            Node* temp = helpRoot->right;
+            delete helpRoot->right;
+            return temp;
+        }else if(helpRoot->right == nullptr) {
+           Node* temp = helpRoot->left;
+            delete helpRoot;
+            return temp;
+        }
+    }
+    while (helpRoot->right->left != nullptr) {
+        helpRoot->right = helpRoot->right->left;
+    }
+    helpRoot->val = helpRoot->right->val;
+    helpRoot->name = helpRoot->right->name;
+    helpRoot->right = helperRemove(helpRoot->right, helpRoot->right->val);
+    return helpRoot;
+};
+
+//BEGIN REAL FUNCTIONS
+
+string AVLTree::insert(const string& name, int id) {
+    if (helperInsert(root, name, id) == nullptr) {
+        return "unsuccessful \n";
+
+    }
+    helperInsert(root, name, id);
+    return "successful \n";
+};
+string AVLTree::remove(int id) {
+    if (helperSearch(root, id) == nullptr) {
+        return "unsuccessful \n";
+    }
+    helperRemove(root, id);
+    return "successful \n";
+};
+string AVLTree::searchId(int id) {
+    if (helperSearch(root, id) == nullptr) {
+        return "unsuccessful \n";
+    }
+    helperSearch(root, id);
+    return "successful \n";
+};
+
+string AVLTree::searchName(const string& name) {
+    vector<string> vals;
+    string final;
+    helperSearchname(root, name, vals);
+    if (vals.empty()) {
+        return "unsuccesful \n";
+    }
+    for (int i = 0; i < (int)vals.size(); i++) {
+        final += vals[i];
+        final += "\n";
+    }
+    return "successful \n";
+};
+
+void AVLTree::printInorder() {
+    helperInorder(root);
+};
+
+void AVLTree::printPreorder() {
+    helperPreorder(root);
+};
+
+void AVLTree::printPostorder() {
+    helperPostorder(root);
+};
+
+int AVLTree::printLevelCount() {
+    return helperMax(root);
+};
+
+string AVLTree::removeInorder(int n) {
+    vector<int> vals;
+    helperInordersort(root, vals);
+    if (n < 0) {
+        return "unsuccessful \n";
+    }
+    if (n > (int)vals.size()) {
+        return "unsuccessful \n";
+    }
+    helperRemove(root, vals[n]);
+    return "successful \n";
+};
+
